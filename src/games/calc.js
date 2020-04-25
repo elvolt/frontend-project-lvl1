286943ab1de615932130064c1c3d136f1bba65f0
elvolt <sidelevCS@gmail.com>
2020-04-25 21:10:41 +0300

    refactor. add getArithmeticResult()

--- src/games/calc.js
+++ src/games/calc.js
@@ -6,27 +6,25 @@ const TASK = 'What is the result of the expression?';
 const MIN_NUM = 0;
 const MAX_NUM = 100;
 
+const getArithmeticResult = (operand1, operand2, operator) => {
+  switch (operator) {
+    case '+':
+      return operand1 + operand2;
+    case '-':
+      return operand1 - operand2;
+    case '*':
+      return operand1 * operand2;
+    default:
+      throw new Error(`Unknown opertor: '${operator}'`);
+  }
+};
+
 const generateQuestionAndRightAnswer = () => {
   const operand1 = getRandomInt(MIN_NUM, MAX_NUM);
   const operand2 = getRandomInt(MIN_NUM, MAX_NUM);
-  // const operator = OPERATORS[Math.floor(Math.random() * OPERATORS.length)];
   const operator = OPERATORS[getRandomInt(0, OPERATORS.length - 1)];
 
-  let rightAnswer = 0;
-  switch (operator) {
-    case '+':
-      rightAnswer = operand1 + operand2;
-      break;
-    case '-':
-      rightAnswer = operand1 - operand2;
-      break;
-    case '*':
-      rightAnswer = operand1 * operand2;
-      break;
-
-    // no default
-  }
-
+  const rightAnswer = getArithmeticResult(operand1, operand2, operator);
   const question = `${operand1} ${operator} ${operand2}`;
 
   return [question, String(rightAnswer)];
